5c34b7d51c7bac78d1d3eba2dc66ecf558a88510
Lukas Cao <lukascao06@gmail.com>
2025-04-05 13:13:59 -0700

    Init App

---
+++ treasure/treasure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  treasure
+//
+//  Created by Lukas Cao on 4/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ treasure/treasure/treasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  treasureApp.swift
+//  treasure
+//
+//  Created by Lukas Cao on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct treasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
